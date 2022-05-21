a6ee63e841c0b32ced0366cf61298e847021a13f
A-Marly <a.marly@lahorde.school>
2022-05-21 11:21:45 +0200

    reglement bugs

--- Exiled_Charming/Assets/Scripts/Player/Movements.cs
+++ Exiled_Charming/Assets/Scripts/Player/Movements.cs
@@ -7,13 +7,11 @@ public class Movements : MonoBehaviour
     private Vector3 originPos, targetPos;
     private Vector2 respawnPoint;
 
-    private bool isMoving;
+    public bool isMoving;
 
     private float timeForMoove = 0.2f;
     void Update()
     {
-<<<<<<< HEAD
-
         if (SaveManager.instance.HasLoaded)
         {
            // add saved HP//
@@ -24,39 +22,24 @@ public class Movements : MonoBehaviour
 
         SaveManager.instance.ActiveSave.RespawnPositionSaved = transform.position;
 
-        if (Input.GetKey(KeyCode.Z) && !isMoving)
-=======
         float axeX = Input.GetAxisRaw("Horizontal");
         float axeY = Input.GetAxisRaw("Vertical");
         if (Input.GetKey(KeyCode.Z) && !isMoving /*&& Triggers[0].GetComponent<TriggerMove>().Collid*/ || axeY > 0 && !isMoving)
->>>>>>> Mapping
         {
             StartCoroutine(MovePlayer(Vector3.up));
         }
 
-<<<<<<< HEAD
-        if (Input.GetKey(KeyCode.Q) && !isMoving)
-=======
         if (Input.GetKey(KeyCode.Q) && !isMoving /*&& Triggers[3].GetComponent<TriggerMove>().Collid*/ || axeX < 0 && !isMoving)
->>>>>>> Mapping
         {
             StartCoroutine(MovePlayer(Vector3.left));
         }
 
-<<<<<<< HEAD
-        if (Input.GetKey(KeyCode.S) && !isMoving)
-=======
         if (Input.GetKey(KeyCode.S) && !isMoving /*&& Triggers[2].GetComponent<TriggerMove>().Collid*/ || axeY < 0 && !isMoving)
->>>>>>> Mapping
         {
             StartCoroutine(MovePlayer(Vector3.down));
         }
 
-<<<<<<< HEAD
-        if (Input.GetKey(KeyCode.D) && !isMoving)
-=======
         if (Input.GetKey(KeyCode.D) && !isMoving /*&& Triggers[1].GetComponent<TriggerMove>().Collid*/ || axeX > 0 && !isMoving)
->>>>>>> Mapping
         {
             StartCoroutine(MovePlayer(Vector3.right));
         }
