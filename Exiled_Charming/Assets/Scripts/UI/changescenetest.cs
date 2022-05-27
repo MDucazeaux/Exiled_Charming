073eaa928fce15466e1ddbdc919f057c4b891e5c
thomas magalhaes <t.magalhaes@lahorde.school>
2022-05-27 08:10:52 +0200

    finition on test, fermer menu

---
+++ Exiled_Charming/Assets/Scripts/UI/changescenetest.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class changescenetest : MonoBehaviour
+{
+    public GameObject Object;
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Object.SetActive(true);
+        }
+    }
+
+    public void switchscene(string name)
+    {
+        SceneManager.LoadScene(name, LoadSceneMode.Single);
+    }
+}
