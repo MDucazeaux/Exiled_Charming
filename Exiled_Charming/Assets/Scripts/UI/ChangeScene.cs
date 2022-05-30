6b043ffd8211edc1ddb720317e75f402f986cbfe
A-Marly <a.marly@lahorde.school>
2022-05-30 17:29:13 +0200

    thomas

--- Exiled_Charming/Assets/Scripts/UI/ChangeScene.cs
+++ Exiled_Charming/Assets/Scripts/UI/ChangeScene.cs
@@ -7,11 +7,56 @@ using UnityEngine.SceneManagement;
 // le change scene du menu qui permet d'appellé la bonne scène graçe a un string dans le on click
 public class changescene : MonoBehaviour
 {
-    public void switchscene(string name)
+
+    private float timer;
+
+    bool activescene2;
+    bool activescene3;
+    
+    public void Start()
     {
-        SceneManager.LoadScene(name, LoadSceneMode.Single);
+        timer = 0.7f;
     }
 
+    public void Update()
+    {
+        if (activescene2 == true)
+        {
+            timer -= Time.deltaTime;
+
+            if (timer <= 0)
+            {
+                SceneManager.LoadScene("Credit", LoadSceneMode.Single);
+                timer = 0.7f;
+            }
+        }
+
+        if (activescene3 == true)
+        {
+            timer -= Time.deltaTime;
+
+            if (timer <= 0)
+            {
+                SceneManager.LoadScene("Menu", LoadSceneMode.Single);
+                timer = 0.7f;
+            }
+        }
+    }
+
+    public void switchscene()
+    {
+        SceneManager.LoadScene("Test", LoadSceneMode.Single);
+    }
+
+    public void switchscene2()
+    {
+        activescene2 = true;
+    }
+    public void switchscene3()
+    {
+       activescene3 = true;
+    }
+   
     public void quitGame()
     {
         Application.Quit();
